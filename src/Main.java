dd701c32a2cb1de3fa7f8ae106210a3eeb14d1f3
SmthInUrEye <dragon-pg@yandex.ru>
2024-09-12 23:15:54 +0300

    Homework4

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+int variable1 = 5;
+double variable2 = 2.5;
+long variable3 =  2500;
+byte variable4 = 1;
+float variable5 = 24.23f;
+short variable6 = 3056;
+
+//Задача №1
+
+        System.out.println(
+                " Значение переменной variable1" + " с типом int равно " + variable1 +
+                "\n Значение переменной variable2" + " с типом double равно " + variable2 +
+                "\n Значение переменной variable3" + " с типом long равно " + variable3 +
+                "\n Значение переменной variable4" + " с типом byte равно " + variable4 +
+                "\n Значение переменной variable5" + " с типом float равно " + variable5 +
+                "\n Значение переменной variable6" + " с типом short равно " + variable6
+        );
+
+//Задача №2
+
+double var1 = 27.12;
+long var2 = 987678965549l;
+float var3 = 2.786f;
+int var4 = 569;
+int var5 = -159;
+short var6 = 27897;
+short var7 = 67;
+
+//Задача №3
+
+int studentsLP = 23, studentsAS = 27, studentsEA = 30, paperQTY = 480;
+        System.out.println("\n На каждого ученика рассчитано " + (paperQTY/(studentsLP+studentsAS+ studentsEA)) + " листов бумаги");
+
+//Задача №4
+
+int productivity = 16/2, workTime = 0;
+
+    }
+}
