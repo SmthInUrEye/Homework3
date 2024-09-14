910c9a301c30bfa0921dccfc3f0022550d58d01b
SmthInUrEye <dragon-pg@yandex.ru>
2024-09-14 09:24:46 +0300

    Homework3

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
 int variable1 = 5;
 double variable2 = 2.5;
-long variable3 =  2500;
+long variable3 =  2500L;
 byte variable4 = 1;
 float variable5 = 24.23f;
 short variable6 = 3056;
@@ -23,7 +23,7 @@ short variable6 = 3056;
 
 //Задача №2
 double var1 = 27.12;
-long var2 = 987678965549l;
+long var2 = 987678965549L;
 float var3 = 2.786f;
 int var4 = 569;
 int var5 = -159;
@@ -39,10 +39,10 @@ int studentsLP = 23, studentsAS = 27, studentsEA = 30, paperQTY = 480;
 
 int productivity = 16/2, workTime = 0;
         System.out.println(
-         "\n За " + (workTime = 20) + " часов машина произвела " + (productivity*workTime) +" штук бутылок" +
-         "\n За " + (workTime = 24*60) + " часов машина произвела " + (productivity*workTime) +" штук бутылок" +
-         "\n За " + (workTime = 24*3*60) + " часов машина произвела " + (productivity*workTime) +" штук бутылок" +
-         "\n За " + (workTime = 24*30*60) + " часов машина произвела " + (productivity*workTime) +" штук бутылок" );
+         "\n За " + (workTime = 20) + " минут машина произвела " + (productivity*workTime) +" штук бутылок" +
+         "\n За " + (workTime = 24*60) + " минут машина произвела " + (productivity*workTime) +" штук бутылок" +
+         "\n За " + (workTime = 24*3*60) + " минут машина произвела " + (productivity*workTime) +" штук бутылок" +
+         "\n За " + (workTime = 24*30*60) + " минут машина произвела " + (productivity*workTime) +" штук бутылок" );
 
 //Задача №5
 
@@ -59,17 +59,16 @@ double bananasWeigth = 80, milkWeigth = 105, iceCreamWeigth = 100, eggsWeigth = 
 
 //Задача №7
 int targetWeigth = 7*1000, UpperLimitPerDay = 500, LowerLimitPerDay = 250;
-        System.out.println("\n Количество необходимых дней если худеть по 250 грамм в день - " + targetWeigth/UpperLimitPerDay
-         + "\n Количество необходимый дней если худеть по 500 грамм в день - " + targetWeigth/LowerLimitPerDay +
+        System.out.println("\n Количество необходимых дней если худеть по 500 грамм в день - " + targetWeigth/UpperLimitPerDay
+         + "\n Количество необходимый дней если худеть по 250 грамм в день - " + targetWeigth/LowerLimitPerDay +
         "\n В среднем количество дней для похудения -  " + ((targetWeigth/UpperLimitPerDay) + (targetWeigth/UpperLimitPerDay)/2));
 
 //Задача №8
-int salaryM = 67760, salaryD = 83690, salaryK = 76230;
+int salaryM = 67760, salaryD = 83294, salaryK = 76230;
         System.out.println(
                 "\n Маша теперь получает " + salaryM*1.1 + ". Годовой доход вырос на " + (salaryM*1.1 - salaryM)*12 + " рублей" +
                 "\n Денис теперь получает " + salaryD*1.1 + ". Годовой доход вырос на " + (salaryD*1.1 - salaryD)*12 + " рублей" +
                 "\n Кристина теперь получает " + salaryK*1.1 + ". Годовой доход вырос на " + (salaryK*1.1 - salaryK)*12 + " рублей"
         );
-
     }
 }
